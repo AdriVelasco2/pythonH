154f6abda12fe190722135e2a5f02f2a700a4cc6
Adrian Velasco Costa <avelascoc@hiberus.com>
2024-12-10 13:30:05 +0100

    Tuples added

---
+++ Tulas/baraja.py
@@ -0,0 +1,13 @@
+palos=["Corazones", "Picas", "Diamantes", "Trébol"]
+cartas=["As","2", "3", "4", "5", "6", "7", "8","9","10","J","Q","K" ]
+
+# for valor, palo in zip(cartas, palos):
+#     print(valor, palo)
+    
+
+for valor in cartas:
+    for palo in palos:
+        print (valor, palo)
+        
+
+# Sé que no es ideal la representación de la baraja pero es válida.
---
+++ Tulas/poker.py
@@ -0,0 +1,29 @@
+import random
+
+
+palos=("Corazones", "Picas", "Diamantes", "Trébol")
+cartas=("As","2", "3", "4", "5", "6", "7", "8","9","10","J","Q","K" )
+baraja= []
+mano=[]
+comprobar=[]
+contador=0
+for valor in cartas:
+    for palo in palos:
+        baraja.append(f"{valor} de {palo}")
+
+for i in range(4):
+    aleatorio= random.choice(baraja)
+    mano.append(aleatorio)
+    
+poker= [valor.split(" de ")[0]for valor in mano]
+
+for i in range(len(poker)):
+    comprobar.append(poker[i])
+    if comprobar[i]==poker[i]:
+        contador+1
+        
+if contador==4:
+    print("Póker!!")
+else:
+    print("Te has arruinado")
+print(poker)
